3a9febc6a86bceaf9a2bddb13a4f1a7d3504da26
Sajjad55555 <140962256+Sajjad55555@users.noreply.github.com>
2025-02-12 16:10:31 +0500

    some changes footer

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
       {/* Footer */}
       <div><Footer /></div>
       {/* model */}
-      <div className="mt-[110px] md:hidden "><Model_Handel /></div>
+      <div className="mt-[97px] md:hidden "><Model_Handel /></div>
 
       {/* Insurance Popup */}
     </div>
